5c6f4253ab77bc2ccccef18b9bfc0cec06ad0bff
davidpengsu <winddycountry01@gmail.com>
2025-07-17 17:31:43 +0900

    Update BoardApplication.java

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.example.board.mapper")  // 이게 핵심!!!
+@MapperScan("com.example.board.mapper")  
 @SpringBootApplication
 public class BoardApplication {
 	public static void main(String[] args) {
